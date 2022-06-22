a67398e93d1e4551a75c47c5e162cf5a53d79835
joy <mahadihasanjoy95@gmail.com>
2022-06-22 22:55:22 +0600

    SIGNUP INIT

--- src/component/SignUpForm.js
+++ src/component/SignUpForm.js
@@ -1,92 +1,64 @@
 import React from 'react';
 import {Formik} from 'formik';
 import {useNavigate} from 'react-router-dom'
+import * as Yup from 'yup';
 
 export default function SignUpForm() {
-    const validate = values => {
-        const errors = {};
-        if (!values.firstName) {
-            errors.firstName = 'Required';
-        } else if (values.firstName.length > 15) {
-            errors.firstName = 'Must be 15 characters or less';
-        }
 
-        if (!values.lastName) {
-            errors.lastName = 'Required';
-        } else if (values.lastName.length > 20) {
-            errors.lastName = 'Must be 20 characters or less';
-        }
-
-        if (!values.email) {
-            errors.email = 'Required';
-        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-            errors.email = 'Invalid email address';
-        }
-
-        console.log(errors)
-        return errors;
-    };
 
     const navigate = useNavigate();
-    // const formik = useFormik({
-    //     initialValues: {
-    //         email: "lala@g.c",
-    //         firstName: "Mahadi Hasan",
-    //         lastName:"Joy",
-    //         password:"********"
-    //     },
-    //     validate
-    //     ,
-    //     onSubmit: values => {
-    //         // console.log(formik.values.email, formik.values.firstName, formik.values.lastName, formik.values.password)
-    //         navigate('/signIn',{replace: true})
-    //     },
-    // });
-    return (
-        <Formik initialValues={{
-            email: "lala@g.c", firstName: "Mahadi Hasan",
-            lastName: "Joy",
-            password: "********"
-        }}
-                onSubmit={(values, {setSubmitting}) => {
-                    setTimeout(() => {
-                        setSubmitting(false);
-                    }, 400);
-                }}>
-            {formik => (
-                <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="email">Email Address</label>
-                    <input
-                        id="email"
-                        type="email"
-                        {...formik.getFieldProps("email")}
-                    />
-                    {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}<br/>
-                    <label htmlFor="firstName">First Name</label>
-                    <input
-                        id="firstName"
-                        type="text"
-                        {...formik.getFieldProps("firstName")}
-                    />
-                    {formik.touched.firstName && formik.errors.firstName ?
-                        <div>{formik.errors.firstName}</div> : null}<br/>
-                    <label htmlFor="lastName">Last Name</label>
-                    <input
-                        id="lastName"
-                        type="text"
-                        {...formik.getFieldProps("lastName")}
-                    />
-                    {formik.touched.lastName && formik.errors.lastName ?
-                        <div>{formik.errors.lastName}</div> : null}<br/>
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        {...formik.getFieldProps("password")}
-                    /><br/>
-                    <button type="submit">Submit</button>
-                </form>
-            )}
+    return (<Formik
+            initialValues={{
+                email: "lala@g.c", firstName: "Mahadi Hasan", lastName: "Joy", password: "********"
+            }}
+            validationSchema={Yup.object({
+                firstName:Yup.string().min(2,"Too Short").max(25,"Too Many").required("Required*"),
+                lastName:Yup.string().min(2,"Too Short").max(25,"Too Many"),
+                email:Yup.string().email("Wrong Email Format").required("Required*"),
+                password: Yup.string()
+                    .required('No password provided.')
+                    .min(8, 'Password is too short - should be 8 chars minimum.')
+                    .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')            })}
+            onSubmit={(values, {setSubmitting}) => {
+                setTimeout(() => {
+                    navigate("/signIn")
+                    alert(JSON.stringify(values,null,10))
+                    setSubmitting(false);
+                }, 400);
+            }}>
+            {formik => (<form onSubmit={formik.handleSubmit}>
+                <label htmlFor="email">Email Address</label>
+                <input
+                    id="email"
+                    type="email"
+                    {...formik.getFieldProps("email")}
+                />
+                {formik.touched.email && formik.errors.email ? <div>{formik.errors.email}</div> : null}<br/>
+                <label htmlFor="firstName">First Name</label>
+                <input
+                    id="firstName"
+                    type="text"
+                    {...formik.getFieldProps("firstName")}
+                />
+                {formik.touched.firstName && formik.errors.firstName ?
+                    <div>{formik.errors.firstName}</div> : null}<br/>
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                    id="lastName"
+                    type="text"
+                    {...formik.getFieldProps("lastName")}
+                />
+                {formik.touched.lastName && formik.errors.lastName ?
+                    <div>{formik.errors.lastName}</div> : null}<br/>
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    type="password"
+                    {...formik.getFieldProps("password")}
+                />{formik.touched.password && formik.errors.password ?
+                <div>{formik.errors.password}</div> : null}<br/>
+                <button type="submit">Submit</button>
+            </form>)}
         </Formik>
 
     );
